a2c7bd46d400afa773f380c6225c3efd06c9f641
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-12-06 02:42:36 +0530

    Add files via upload

---
+++ 2_data_types.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //as we are using nodejs, not browser
+
+let naame="Ritik";
+let age=200;
+let isLoggedIn=true;
+let state="Bihar";
+
+console.table([naame,age,isLoggedIn,state]);
+
+// null => standalone value
+// undefined
+// symbol => uniqueness
+
+
+console.log(typeof "Ritik");
+console.log(typeof 200);
---
+++ 3_conversion.js
@@ -0,0 +1,116 @@
+let score="55"
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+console.log("Null");
+
+let scoree=null
+console.log(typeof scoree);
+console.log(typeof (scoree));
+let valueInNumberr=Number(scoree)
+console.log(typeof valueInNumberr);
+console.log(valueInNumberr);
+
+console.log("Null");
+
+let scorree=null
+console.log(typeof scorree);~
+console.log(typeof (scorree));
+let valueInNumbeerr=Number(scorree)
+console.log(typeof valueInNumbeerr);
+console.log(valueInNumbeerr);
+
+console.log("True");
+
+let scoorree=true
+console.log(typeof scoorree);~
+console.log(typeof (scoorree));
+let valueInNumbbeerr=Number(scoorree)
+console.log(typeof valueInNumbbeerr);
+console.log(valueInNumbbeerr);
+
+console.log("Rahul");
+
+let sccoorree="Rahul"
+console.log(typeof sccoorree);~
+console.log(typeof (scoorree));
+let valueInNummbbeerr=Number(sccoorree)
+console.log(typeof valueInNummbbeerr);
+console.log(valueInNummbbeerr);
+
+
+//"33"=>33
+//"33anc"=>NaN
+// true=>1 and flase=>0
+
+console.log("LoogedIn");
+
+let isLoogedIn = 1;
+
+let booleanLoggedIN = Boolean(isLoogedIn);
+console.log(booleanLoggedIN);
+
+//1=>true && 0=>flase
+//""=>false
+//Rahul=>true
+
+console.log("SomeNumber");
+
+let SomeNumber=22
+
+let stringNumber=String(SomeNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// ************Operations*****************
+
+console.log("Operations");
+
+let value=3;
+let negValue= -value;
+
+console.log(negValue);
+
+let str1="Hello";
+let str2=" World";
+
+let str3=str1 + str2;
+console.log(str3);
+
+console.log(2+2)
+console.log("2"+2)
+console.log(2+"2")
+console.log(2+2+"6")
+console.log("2"+2+6)
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+
+let num1, num2, num3;
+num1=num2=num3=2+2  //writing like this is not good
+
+let gameCounter=100;
+
+gameCounter++;
+console.log(gameCounter);
+
+let gameCoounter=200;
+++gameCoounter;
+console.log(gameCoounter)
+
+/* **********Comparision************ */
+console.log(2>1);
+console.log(2<=1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2!=1);
+
+
+
+
